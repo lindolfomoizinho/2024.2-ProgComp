d9aac72de666f3be50e20cd99fc85eefee6b0c46
Lindolfo Moizinho <Lindolfo Moizinho>
2024-11-29 20:21:15 -0300

    Atividade resolvida

--- 2024-11-29 - Laços de Repetição/exemplo_01.py
+++ 2024-11-29 - Laços de Repetição/exemplo_01.py
@@ -3,4 +3,11 @@
    inteiro e informe se o número é par ou ímpar. 
    
    O programa só deve encerrar quando o usuário digitar o número 0.
+
 '''
+while True:
+   n = int(input("Digite um número inteiro: "))
+   if n == 0: break
+   elif n % 2 == 0:
+       print(f"O {n} é um número par")
+   else: print(f"O {n} é um número impar")
--- 2024-11-29 - Laços de Repetição/exemplo_02.py
+++ 2024-11-29 - Laços de Repetição/exemplo_02.py
@@ -6,3 +6,13 @@
 
    O valor 0 não deve entrar na média.
 '''
+
+count = 0
+m = 0
+while True:
+   n = int(input("Digite um número diferente de 0: "))
+   if n == 0:
+      break
+   m += n
+   count += 1
+print(f"A soma de todos os número foi {m} e a média aritimética é {m/count}")
