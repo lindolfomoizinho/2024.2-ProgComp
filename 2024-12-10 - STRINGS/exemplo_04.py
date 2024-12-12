417f049265dce29a1502fd93478e13bf3e90e2b8
Lindolfo Moizinho <lindolfomoizinho@gmail.com>
2024-12-11 21:24:49 -0300

    Exemplo resolvido

--- 2024-12-10 - STRINGS/exemplo_04.py
+++ 2024-12-10 - STRINGS/exemplo_04.py
@@ -3,3 +3,14 @@
    Fazer um programa em que o usuário informe uma palavra e em seguida
    o programa exiba a palavra invertida e diga se ela é um palíndromo ou não.
 '''
+
+palavra = input("Informe uma palavra: ")
+
+palavraInv = ""
+for i in range(len(palavra)-1, -1, -1):
+    palavraInv += palavra[i]
+     
+
+if palavra == palavraInv: print(f"A palavra {palavra} é um palindromo")
+
+else: print(f"A palavra {palavra} não é um palindromo")
--- 2024-12-10 - STRINGS/exemplo_05.py
+++ 2024-12-10 - STRINGS/exemplo_05.py
@@ -3,3 +3,10 @@
    Fazer um programa em que o usuário informe uma string e depois uma palavra
    e em seguida o programa informe se a palavra informada está na string informada
 '''
+
+frase = input("Digite uma frase: ")
+
+palavra = input("Digite uma palavra: ")
+
+if palavra in frase: print(f"A string {palavra} está na frase: {frase}")
+else : print(f"A string {palavra} não está na frase: {frase}")
